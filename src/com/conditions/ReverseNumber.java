efaebf836b44ea0a3fc3c157f44f5f9407bc27a0
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2021-08-11 17:42:11 +0530

    Solution of "Reverse the array" added

---
+++ src/com/conditions/ReverseNumber.java
@@ -0,0 +1,21 @@
+package com.conditions;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        System.out.print("Enter the original number:");
+        Scanner input= new Scanner(System.in);
+        long n = input.nextLong();
+        long ans=0;
+        while(n>0){
+            long k;
+
+            k= n%10;
+            n=n/10;
+            ans=ans*10+k;
+
+        }
+        System.out.println("array in reverse :"+ ans);
+    }
+}
