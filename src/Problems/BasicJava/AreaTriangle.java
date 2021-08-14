20e58053619a39b60d7308c3447e5ed6add2852b
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2021-08-14 20:49:38 +0530

    basic problem 2 solved

---
+++ src/Problems/BasicJava/AreaTriangle.java
@@ -0,0 +1,15 @@
+package Problems.BasicJava;
+
+import java.util.Scanner;
+
+public class AreaTriangle {
+    public static void main(String[] args) {
+
+        System.out.print("Enter the base and height of the triangle:");
+        Scanner input=new Scanner (System.in);
+        double n= input.nextInt();
+        double m= input.nextInt();
+        System.out.print("The area of the triangle is: "+ (n*m)/2);
+
+    }
+}
