ba791ca98e2d7a2358ed17ffb93cd10d249a5405
Randrita Sarkar <60352282+Randrita@users.noreply.github.com>
2021-08-24 20:31:47 +0530

    function basic display problem added

---
+++ src/com/functions/Greetings.java
@@ -0,0 +1,13 @@
+package com.functions;
+
+public class Greetings {
+    public static void main(String[] args) {
+        display();
+        display();
+        display();
+    }
+
+    static void display(){
+        System.out.println("Hello World!");
+    }
+}
